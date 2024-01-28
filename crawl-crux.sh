e8193001cde3f1f289a551ae9e3c6810db8f7a5e
Yohan Beugin <yohan@beugin.org>
2024-01-28 13:04:58 -0600

    Optimize Parallel Command

--- crawl-crux.sh
+++ crawl-crux.sh
@@ -36,9 +36,9 @@ results_crawl_dir=$results_dir/$crawl_time
 mkdir -p $results_crawl_dir
 
 # https://www.gnu.org/software/parallel/parallel_examples.html#example-speeding-up-fast-jobs
-parallel --pipepart -a $crux_origins -j32 -q parallel -j0 -X -N20 ./crawl_origins.sh $results_crawl_dir
+parallel --pipepart -a $crux_origins -j32 --roundrobin -q parallel -j0 -X -N20 ./crawl_origins.sh $results_crawl_dir
 
-# cd $results_dir
-# tar --zstd -c $crawl_time | aws s3 cp - s3://$s3_bucket_name/$crawl_time.tar.zst
-# # rm -r $crawl_time
-# cd ..
+cd $results_dir
+tar --zstd -c $crawl_time | aws s3 cp - s3://$s3_bucket_name/$crawl_time.tar.zst
+rm -r $crawl_time
+cd ..
